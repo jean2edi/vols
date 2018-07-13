c2a01f2d3755b66216b125c9fbe8eb7ac9090786
Jean <jean.2edi@gmail.com>
2018-07-13 15:19:01 +0200

    test en trop

--- src/test/java/com/hb/formation/vols/TestVols.java
+++ src/test/java/com/hb/formation/vols/TestVols.java
@@ -1,13 +1,18 @@
 package com.hb.formation.vols;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import com.hb.formation.vols.datas.Vols;
+import com.hb.formation.vols.model.Place;
 
 public class TestVols {
 
@@ -31,5 +36,21 @@ public class TestVols {
     @Test
     public void test3() {
         assertNotNull(vols.getVol(1));
+    }
+    @Test
+    public void test4() {
+    	List<Place> places = vols.getVol(2).getPlaces();
+    	boolean trouve = false;
+    	for (Place place : places) {
+			if(place.getVente() == 0) {
+				trouve = true;
+			}
+		}
+    	assertTrue(trouve);
+    }
+    @Test
+    public void test5() {
+    	List<Place> places = vols.getVol(2).getPlaces();
+    	assertFalse(places.stream().anyMatch(place -> place.getVente() == 0));
     }
 }
